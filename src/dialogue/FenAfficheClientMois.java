f3ce1bf3b56c9effd10b5ce1ba5109ea0d0ccca1
Arejdal-A <Abdelarejdal@gmail.com>
2014-12-09 15:29:38 +0100

    Creation FenAfficheClientMois fini

--- src/dialogue/FenAfficheClientMois.java
+++ src/dialogue/FenAfficheClientMois.java
@@ -39,34 +39,8 @@ public class FenAfficheClientMois extends javax.swing.JFrame implements TableMod
         jLab_Exporter = new javax.swing.JLabel();
         jLab_Retour = new javax.swing.JLabel();
         jLabTitreClient = new javax.swing.JLabel();
-        jLab_SousTitre = new javax.swing.JLabel();
         jLab_Trier = new javax.swing.JLabel();
         jCmBox_Trier = new javax.swing.JComboBox();
-        jLab_Code = new javax.swing.JLabel();
-        jLab_Nom = new javax.swing.JLabel();
-        jLab_Adresse = new javax.swing.JLabel();
-        jLab_CP = new javax.swing.JLabel();
-        jLab_Telephone = new javax.swing.JLabel();
-        jLab_Email = new javax.swing.JLabel();
-        jLab_Remarques = new javax.swing.JLabel();
-        jTxTCode = new javax.swing.JTextField();
-        jTxTNom = new javax.swing.JTextField();
-        jTXT_Adresse = new javax.swing.JTextField();
-        jTXT_CP = new javax.swing.JTextField();
-        jTXT_Telephone = new javax.swing.JTextField();
-        jTXT_Email = new javax.swing.JTextField();
-        jScrollPane2 = new javax.swing.JScrollPane();
-        jTXA_Remarque = new javax.swing.JTextArea();
-        jLab_DateCreation = new javax.swing.JLabel();
-        jLab_Prenom = new javax.swing.JLabel();
-        jLab_Carte = new javax.swing.JLabel();
-        jTxT_DateCreation = new javax.swing.JTextField();
-        jTxT_Prenom = new javax.swing.JTextField();
-        jLab_Ville = new javax.swing.JLabel();
-        jLab_Mobilis = new javax.swing.JLabel();
-        jTXT_Ville = new javax.swing.JTextField();
-        jTXT_Mobilis = new javax.swing.JTextField();
-        checkBox_Carte = new java.awt.Checkbox();
         jLab_fond_module = new javax.swing.JLabel();
         jLab_Fond = new javax.swing.JLabel();
 
@@ -82,7 +56,7 @@ public class FenAfficheClientMois extends javax.swing.JFrame implements TableMod
         });
         jScrollPane1.setViewportView(table);
 
-        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 350, 742, 135));
+        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 70, 742, 280));
 
         jLab_Ajouter.setForeground(new java.awt.Color(255, 255, 255));
         jLab_Ajouter.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/gestion/ajouter.png"))); // NOI18N
@@ -160,10 +134,6 @@ public class FenAfficheClientMois extends javax.swing.JFrame implements TableMod
         jLabTitreClient.setToolTipText("");
         getContentPane().add(jLabTitreClient, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 10, -1, -1));
 
-        jLab_SousTitre.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        jLab_SousTitre.setText("A j o u t,  m o d i f i c a t i o n,  su p p r e s s i o n,  c o n s u l t a t i o n   e t   i m p r e s s i o n");
-        getContentPane().add(jLab_SousTitre, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 70, -1, -1));
-
         jLab_Trier.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/gestion/trier.png"))); // NOI18N
         jLab_Trier.setText("Trier");
         getContentPane().add(jLab_Trier, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 490, 100, -1));
@@ -171,90 +141,8 @@ public class FenAfficheClientMois extends javax.swing.JFrame implements TableMod
         jCmBox_Trier.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Code", "Nom", "Date de création", "Carte" }));
         getContentPane().add(jCmBox_Trier, new org.netbeans.lib.awtextra.AbsoluteConstraints(520, 500, 130, -1));
 
-        jLab_Code.setText("Code");
-        getContentPane().add(jLab_Code, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 100, 90, -1));
-
-        jLab_Nom.setText("Nom");
-        getContentPane().add(jLab_Nom, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 130, 90, -1));
-
-        jLab_Adresse.setText("Adresse");
-        getContentPane().add(jLab_Adresse, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 160, 90, -1));
-
-        jLab_CP.setText("Code postal");
-        getContentPane().add(jLab_CP, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 190, 90, -1));
-
-        jLab_Telephone.setText("Tél. fixe");
-        getContentPane().add(jLab_Telephone, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 220, 80, -1));
-
-        jLab_Email.setText("Email");
-        getContentPane().add(jLab_Email, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 250, -1, -1));
-
-        jLab_Remarques.setText("Remarques");
-        getContentPane().add(jLab_Remarques, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 280, 80, -1));
-
-        jTxTCode.setEditable(false);
-        getContentPane().add(jTxTCode, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 100, 240, -1));
-
-        jTxTNom.setEditable(false);
-        getContentPane().add(jTxTNom, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 130, 240, -1));
-
-        jTXT_Adresse.setEditable(false);
-        jTXT_Adresse.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTXT_AdresseActionPerformed(evt);
-            }
-        });
-        getContentPane().add(jTXT_Adresse, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 160, 600, -1));
-
-        jTXT_CP.setEditable(false);
-        getContentPane().add(jTXT_CP, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 190, 240, -1));
-
-        jTXT_Telephone.setEditable(false);
-        getContentPane().add(jTXT_Telephone, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 220, 240, -1));
-
-        jTXT_Email.setEditable(false);
-        getContentPane().add(jTXT_Email, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 250, 240, -1));
-
-        jTXA_Remarque.setColumns(20);
-        jTXA_Remarque.setEditable(false);
-        jTXA_Remarque.setRows(5);
-        jScrollPane2.setViewportView(jTXA_Remarque);
-
-        getContentPane().add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 280, 620, 60));
-
-        jLab_DateCreation.setText("Date création");
-        getContentPane().add(jLab_DateCreation, new org.netbeans.lib.awtextra.AbsoluteConstraints(620, 100, 80, -1));
-
-        jLab_Prenom.setText("Prénom");
-        getContentPane().add(jLab_Prenom, new org.netbeans.lib.awtextra.AbsoluteConstraints(620, 130, 80, -1));
-
-        jLab_Carte.setText("Carte fidélité");
-        getContentPane().add(jLab_Carte, new org.netbeans.lib.awtextra.AbsoluteConstraints(840, 100, 80, -1));
-
-        jTxT_DateCreation.setEditable(false);
-        jTxT_DateCreation.setHorizontalAlignment(javax.swing.JTextField.CENTER);
-        getContentPane().add(jTxT_DateCreation, new org.netbeans.lib.awtextra.AbsoluteConstraints(700, 100, 120, -1));
-
-        jTxT_Prenom.setEditable(false);
-        getContentPane().add(jTxT_Prenom, new org.netbeans.lib.awtextra.AbsoluteConstraints(700, 130, 250, -1));
-
-        jLab_Ville.setText("Ville");
-        getContentPane().add(jLab_Ville, new org.netbeans.lib.awtextra.AbsoluteConstraints(620, 190, -1, -1));
-
-        jLab_Mobilis.setText("Mobilis");
-        getContentPane().add(jLab_Mobilis, new org.netbeans.lib.awtextra.AbsoluteConstraints(620, 220, 50, -1));
-
-        jTXT_Ville.setEditable(false);
-        getContentPane().add(jTXT_Ville, new org.netbeans.lib.awtextra.AbsoluteConstraints(700, 190, 250, -1));
-
-        jTXT_Mobilis.setEditable(false);
-        getContentPane().add(jTXT_Mobilis, new org.netbeans.lib.awtextra.AbsoluteConstraints(700, 220, 250, -1));
-
-        checkBox_Carte.setEnabled(false);
-        getContentPane().add(checkBox_Carte, new org.netbeans.lib.awtextra.AbsoluteConstraints(930, 100, 20, -1));
-
         jLab_fond_module.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/gestion/fondModule.png"))); // NOI18N
-        getContentPane().add(jLab_fond_module, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 60, 750, 290));
+        getContentPane().add(jLab_fond_module, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 70, 750, 280));
 
         jLab_Fond.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/fond/fondGeneral.jpg"))); // NOI18N
         getContentPane().add(jLab_Fond, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, -100, 1000, 710));
@@ -349,10 +237,6 @@ public class FenAfficheClientMois extends javax.swing.JFrame implements TableMod
          // code commenté
 //JasperMySQL_Parametres.apercu("EtatClients.jrxml");
     }//GEN-LAST:event_jLab_AperçuMouseClicked
-
-    private void jTXT_AdresseActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTXT_AdresseActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jTXT_AdresseActionPerformed
 
     private void prepaModification() {
         int NumLigne = -1;
@@ -460,46 +344,20 @@ public class FenAfficheClientMois extends javax.swing.JFrame implements TableMod
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private java.awt.Checkbox checkBox_Carte;
     private javax.swing.JComboBox jCmBox_Trier;
     private javax.swing.JLabel jLabTitreClient;
-    private javax.swing.JLabel jLab_Adresse;
     private javax.swing.JLabel jLab_Ajouter;
     private javax.swing.JLabel jLab_Aperçu;
-    private javax.swing.JLabel jLab_CP;
-    private javax.swing.JLabel jLab_Carte;
-    private javax.swing.JLabel jLab_Code;
-    private javax.swing.JLabel jLab_DateCreation;
-    private javax.swing.JLabel jLab_Email;
     private javax.swing.JLabel jLab_Exporter;
     private javax.swing.JLabel jLab_Fond;
     private javax.swing.JLabel jLab_Imprimer;
-    private javax.swing.JLabel jLab_Mobilis;
     private javax.swing.JLabel jLab_Modifier;
-    private javax.swing.JLabel jLab_Nom;
-    private javax.swing.JLabel jLab_Prenom;
     private javax.swing.JLabel jLab_Rechercher;
-    private javax.swing.JLabel jLab_Remarques;
     private javax.swing.JLabel jLab_Retour;
-    private javax.swing.JLabel jLab_SousTitre;
     private javax.swing.JLabel jLab_Supprimer;
-    private javax.swing.JLabel jLab_Telephone;
     private javax.swing.JLabel jLab_Trier;
-    private javax.swing.JLabel jLab_Ville;
     private javax.swing.JLabel jLab_fond_module;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JTextArea jTXA_Remarque;
-    private javax.swing.JTextField jTXT_Adresse;
-    private javax.swing.JTextField jTXT_CP;
-    private javax.swing.JTextField jTXT_Email;
-    private javax.swing.JTextField jTXT_Mobilis;
-    private javax.swing.JTextField jTXT_Telephone;
-    private javax.swing.JTextField jTXT_Ville;
-    private javax.swing.JTextField jTxTCode;
-    private javax.swing.JTextField jTxTNom;
-    private javax.swing.JTextField jTxT_DateCreation;
-    private javax.swing.JTextField jTxT_Prenom;
     private javax.swing.JTable table;
     // End of variables declaration//GEN-END:variables
 }
